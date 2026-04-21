330b50f1e8a2473c3a60ea3e5b04f2a88b40c3ab
sinansevg1 <9a.sinansevgi@gmail.com>
2026-04-21 12:45:42 +0300

    feat(bl): servis interface'lerini ekle
    Hafta 4 kapsaminda BusinessLayer/Abstract altina UdemySignalRProject ile uyumlu servis interface'leri eklendi.

---
+++ SignalR.BusinessLayer/Abstract/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Abstract
+{
+    public interface IBookingService : IGenericService<Booking>
+    {
+        object TGetList();
+        void TBookingStatusApproved(int id);
+        void TBookingStatusCancelled(int id);
+    }
+}
