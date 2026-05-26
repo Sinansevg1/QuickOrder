1e97ed33f5b4f614ea09d1448d2512c998aedd74
sinansevg1 <9a.sinansevgi@gmail.com>
2026-05-26 14:20:47 +0300

    fix: varsayilan sayfa Default/Index olarak guncellendi

--- SignalRWebUI/Program.cs
+++ SignalRWebUI/Program.cs
@@ -80,7 +80,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Default}/{action=Index}/{id?}");
 
 app.Run();
 
